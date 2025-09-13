26952acfb1fcb65d9004f83c69e10ea5e93fc1c6
sonali-varshney <44549506+sonali-varshney@users.noreply.github.com>
2025-09-13 05:50:39 +0530

    Update main.tf

--- microservices/main.tf
+++ microservices/main.tf
@@ -129,9 +129,9 @@ resource "aws_eks_node_group" "eks_node_group" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.eks_node_policy_1,
+    aws_iam_role_policy_attachment.eks_node_policy_2,
+    aws_iam_role_policy_attachment.eks_node_policy_3,
   ]
 }
 
