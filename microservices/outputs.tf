942bc782e9b5fe34b2c3924f24b3191d440ecfd1
sonali-varshney <varshneysonali28@gmail.com>
2025-09-15 06:17:57 +0530

    msg

--- microservices/outputs.tf
+++ microservices/outputs.tf
@@ -1,11 +1,11 @@
 output "cluster_name" {
-  value = module.eks.cluster_id
+  value = aws_eks_cluster.myekscluster #module.eks.cluster_id
 }
 
-output "kubeconfig_command" {
-  value = "aws eks update-kubeconfig --region us-east-1 --name ${module.eks.cluster_id}"
-}
+# output "kubeconfig_command" {
+#   value = "aws eks update-kubeconfig --region us-east-1 --name ${module.eks.cluster_id}"
+# }
 
 output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  value = aws_eks_cluster.myekscluster # module.eks.cluster_endpoint
 }
