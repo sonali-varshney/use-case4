fc88d954a2d40d5ac39258f5029748543d35fdf0
Sonali Varshney <sonali.varshney@hcltech.com>
2025-09-15 10:05:18 +0530

    adding backend

--- microservices/main.tf
+++ microservices/main.tf
@@ -2,6 +2,14 @@ provider "aws"{
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "test-s3-terraform-use-case1" 
+    key    = "dev/terraform.tfstate"    #Note  we have to give terraform.tfstate
+    region = "us-east-1"
+  }
+}
+
 #provider "helm" {
 #    kubernetes = {
 #        host  = data.aws_eks_cluster.eks_cluster.endpoint
